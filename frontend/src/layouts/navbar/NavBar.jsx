64d23985c96d915890b33753b9c589f6e660ab8d
Ayelen <ayelenreinglestein@gmail.com>
2026-02-10 12:06:36 -0300

    Se agrego contador de unidades al carrito del NavBar.

--- frontend/src/layouts/navbar/NavBar.jsx
+++ frontend/src/layouts/navbar/NavBar.jsx
@@ -3,9 +3,12 @@ import iconCart from "../../assets/iconCart.png"
 import { Link } from "react-router-dom"
 import { Search } from '../../components/Search'
 import { useProducts } from "../../context/Product/useProducts";
+import { useCart } from "../../context/Cart/useCart";
 
 export const NavBar = () => {
     const { updateFilter } = useProducts()
+    const { totalUnits } = useCart()
+    
 
     return(
         <div className={styles.containerSearchBar}>
@@ -16,6 +19,7 @@ export const NavBar = () => {
             <Search updateFilter={updateFilter} />
 
             <Link to={`/cart`} className={styles.containerCart}>
+                <p className={styles.pTotalUnits}>{totalUnits}</p>
                 <img src={iconCart} alt="Carrito" className={styles.imgCart}/>
                 <p className={styles.pCart}>Carrito</p>
             </Link>
